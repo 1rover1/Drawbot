3564b15243070342f0f863958817c3d404e5d9c1
1rover1 <hello@joel.ly>
2015-11-14 10:22:37 +1000

    Clean up

--- test.php
+++ test.php
@@ -9,9 +9,9 @@ $config = json_decode(file_get_contents('config/config.json'), true);
 $plt = new Plotter($config);
 
 
-//$plt->penTo(1000,-1000);
-
 $plt->circleRight(600);
 $plt->circleLeft(400);
+
+
 
 echo "Done\n";
