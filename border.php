bef89429a7a8a2f66ea83980c03235e64e647057
1rover1 <joelheinzel@hotmail.com>
2016-01-08 21:11:41 +1000

    Fixed border app, added demo app

--- border.php
+++ border.php
@@ -10,22 +10,13 @@ use Rover2011\AnnDroidArtist\Plotter;
 $config = json_decode(file_get_contents('config/config.json'), true);
 $plt = new Plotter($config);
 
-/*
+
 $plt->drawTo($plt->getWidth(), 0);
 $plt->moveTo(0,                0);
 $plt->drawTo($plt->getWidth(), $plt->getHeight());
 $plt->drawTo(0,                $plt->getHeight());
 $plt->drawTo(0,                0);
-*/
 
-$plt->moveTo(0, 0);
-for($i = 0; $i < 10; $i++) {
-    if(($i % 2) == 1) {
-        $plt->drawTo($plt->getWidth * $i / 10, $plt->getHeight() - $plt->getHeight() * $i / 10);
-    } else {
-        $plt->drawTo($plt->getHeight() - $plt->getHeight() * $i / 10, $plt->getWidth * $i / 10);
-    }
-}
 
 
 echo "Done.\n";
---
+++ demo.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+include('vendor/autoload.php');
+
+use Rover2011\AnnDroidArtist\Plotter;
+
+
+// Create a plotter
+$config = json_decode(file_get_contents('config/config.json'), true);
+$plt = new Plotter($config);
+
+
+
+
+for($i = 0; $i < 10; $i++) {
+    if(($i % 2) == 1) {
+        $plt->moveTo($plt->getWidth * $i / 10, 0);
+        $plt->drawTo(0, $plt->getHeight() - $plt->getHeight() * $i / 10);
+    } else {
+        $plt->moveTo(0, $plt->getHeight() - $plt->getHeight() * $i / 10);
+        $plt->drawTo($plt->getWidth * $i / 10, 0);
+    }
+}
+
+
+
+
+echo "Done.\n";
