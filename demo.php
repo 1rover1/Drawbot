402a7394870bd00eb83cfd9a0f093c7aead78b24
1rover1 <joelheinzel@hotmail.com>
2016-01-09 06:07:35 +1000

    Updated demo (again)

--- demo.php
+++ demo.php
@@ -15,11 +15,11 @@ $plt = new Plotter($config);
 
 for($i = 1; $i <= 10; $i++) {
     if(($i % 2) == 1) {
-        $plt->moveTo($plt->getWidth * $i / 10, 0);
+        $plt->moveTo($plt->getWidth() * $i / 10, 0);
         $plt->drawTo(0, $plt->getHeight() - $plt->getHeight() * $i / 10);
     } else {
         $plt->moveTo(0, $plt->getHeight() - $plt->getHeight() * $i / 10);
-        $plt->drawTo($plt->getWidth * $i / 10, 0);
+        $plt->drawTo($plt->getWidth() * $i / 10, 0);
     }
 }
 
